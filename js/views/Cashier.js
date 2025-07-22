df0ef16b2b1e0e2da76447eb2e76ba286a9dd113
えーぷる <keita023@icloud.com>
2025-07-22 17:28:32 +0900

    調整版

--- js/views/Cashier.js
+++ js/views/Cashier.js
@@ -2,7 +2,7 @@
 import { fetchProducts, postOrder } from '../api.js';
 import { io } from 'https://cdn.socket.io/4.7.5/socket.io.esm.min.js';
 
-const PRINT_SERVER_URL = 'http://fes-printer.local:5001/print';
+const PRINT_SERVER_URL = 'https://fes-printer.local:5001/print';
 const REALTIME_SERVER_URL = 'https://fes-realtime.local:5002';
 
 export const Cashier = {
